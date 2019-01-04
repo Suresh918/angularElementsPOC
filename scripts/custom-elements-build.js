2daa5643e96e84013f2435892d9596ddcdd7c34d
suputhul <suresh.uputhula@asml.com>
2019-01-04 22:59:59 +0530

    custom elements build file updated

--- scripts/custom-elements-build.js
+++ scripts/custom-elements-build.js
@@ -2,10 +2,10 @@ const fs = require('fs-extra');
 const concat = require('concat');
 (async function build() {
   const files = [
-    './dist/runtime.js',
-    './dist/polyfills.js',
-    './dist/4.js',
-    './dist/main.js',
+    './dist/angularElementsPOC/runtime.js',
+    './dist/angularElementsPOC/polyfills.js',
+    './dist/angularElementsPOC/4.js',
+    './dist/angularElementsPOC/main.js',
   ];
   await fs.ensureDir('elements-dist');
   await concat(files, 'elements-dist/custom-elements.js');
